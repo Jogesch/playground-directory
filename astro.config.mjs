197ada26635d5b3bd30c5aa910fd024f6bffb3ef
Jogesch <71102670+Jogesch@users.noreply.github.com>
2025-06-01 20:35:49 +0100

    fix: update dependencies and remove image processing for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
-  ],
-  site: 'https://london-playgrounds.netlify.app', // Update this with your actual domain
-  output: 'static'
+  integrations: [tailwind()],
+  output: 'static',
+  site: 'https://playground-directory.netlify.app',
 }); 
